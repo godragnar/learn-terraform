eb87de2cc1738fe7170a2f7cf97976be98ac33c5
kk_666 <kirankumpatla666@gmail.com>
2023-12-10 15:27:57 +0530

    Terraform

--- resources/sample.tf
+++ resources/sample.tf
@@ -1,15 +1,4 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.30.0"
-    }
-  }
-}
 
-provider "aws" {
-  region = "us-east-1"
-}
 resource "aws_instance" "sample" {
   ami="ami-03265a0778a880afb"
   instance_type = "t3.micro"
