cf3efa672acb2f5495ce2d0aa665d8fecb33f4c9
kk_666 <kirankumpatla666@gmail.com>
2023-12-10 15:42:40 +0530

    Terraform

--- variables/vars.tf
+++ variables/vars.tf
@@ -4,3 +4,14 @@ variable "cars" {
 output "cars_with_first_car" {
   value = var.cars[0]
 }
+
+variable "cars_with_prices" {
+  default = {
+    BMW=10000000
+    Lamborgini=20000000
+    Ferrari=3000000
+  }
+}
+output "cars_ferrari_price" {
+  value = var.cars_with_prices["Ferrari"]
+}
