97b9d37b06b9429e8ac4418f234d73a59df86b7d
kk_666 <kirankumpatla666@gmail.com>
2023-12-16 22:05:22 +0530

    Created EC2 instances and DNS Records using terraform

--- expense-v3/dev.tfvars
+++ expense-v3/dev.tfvars
@@ -2,17 +2,17 @@ env = "dev"
 ami = "ami-03265a0778a880afb"
 sg_id = ["sg-07af6364e76485c77"]
 zone_id = "Z09500041AC5RMFMHJ5ZR"
-components={
-  frontend={
-    name="frontend"
-    instance_type="t3.micro"
+components = {
+  frontend = {
+    name = "frontend"
+    instance_type = "t3.micro"
   }
-  backend={
-    name="backend"
-    instance_type="t3.micro"
+  backend = {
+    name = "backend"
+    instance_type = "t3.micro"
   }
-  mysql= {
-    name          = "mysql"
+  mysql = {
+    name = "mysql"
     instance_type = "t3.micro"
   }
 }
--- expense-v3/main.tf
+++ expense-v3/main.tf
@@ -8,13 +8,14 @@ resource "aws_instance" "instances" {
     Name = "${each.value["name"]}-${var.env}"
   }
 }
-#resource "aws_route53_record" "frontend" {
- # name    = "frontend-${var.env}"
-  #type    = "A"
-  #zone_id = var.zone_id
-  #ttl=30
-  #records = [aws_instance.frontend.private_ip]
-#}
+resource "aws_route53_record" "frontend" {
+  for_each = var.components
+  name     = "${each.value["name"]}${var.env}"
+  type     = "A"
+  zone_id  = var.zone_id
+  ttl      = 30
+   records = [aws_instance.instances[each.value["name"]].private_ip]
+}
 
 
 
