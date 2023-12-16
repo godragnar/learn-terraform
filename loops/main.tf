f874c48bbcb017a57f93d16219053fcb0e3120e2
kk_666 <kirankumpatla666@gmail.com>
2023-12-16 20:51:44 +0530

    Created EC2 instances and DNS Records using terraform

--- loops/main.tf
+++ loops/main.tf
@@ -1,7 +1,27 @@
+
 variable "instances" {
-  default = ["frontend","backend","mysql"]
+  default = ["frontend","backend","mysql"] #Count
 }
 
 resource "null_resource" "sample" {
   count = length(var.instances)
+}
+
+#for-each-if the i/p is a map of values
+
+variable "instances1"{
+  default = {
+    frontend={
+      name="frontend"
+      instance_arch="t3.micro"
+    }
+    backend={
+      name="backend"
+      instance_arch="t3.micro"
+    }
+    mysql={
+      name="mysql"
+      instance_arch="t3.micro"
+    }
+  }
 }
