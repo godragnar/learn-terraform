3664fca945a919110cd53fb321887ebdd0a84783
kk_666 <kirankumpatla666@gmail.com>
2023-12-16 19:25:03 +0530

    Created EC2 instances and DNS Records using terraform

--- expense-v1/main.tf
+++ expense-v1/main.tf
@@ -27,3 +27,28 @@ resource "aws_instance" "mysql" {
     Name = "MySQL"
   }
 }
+
+#Creating DNS Records
+ resource "aws_route53_record" "frontend" {
+   name    = "frontend-dev"
+   type    = "A"
+   zone_id = "Z09500041AC5RMFMHJ5ZR"
+   ttl=30
+   records = [aws_instance.frontend.private_ip]
+ }
+
+resource "aws_route53_record" "backend" {
+  name    = "backend-dev"
+  type    = "A"
+  zone_id = "Z09500041AC5RMFMHJ5ZR"
+  ttl=30
+  records = [aws_instance.backend.private_ip]
+}
+
+resource "aws_route53_record" "mysql" {
+  name    = "mysql-dev"
+  type    = "A"
+  zone_id = "Z09500041AC5RMFMHJ5ZR"
+  ttl=30
+  records = [aws_instance.mysql.private_ip]
+}
