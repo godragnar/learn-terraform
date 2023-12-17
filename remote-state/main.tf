3394a501a79206afab9b1163fa09c2b767a8a7ef
kk_666 <kirankumpatla666@gmail.com>
2023-12-17 10:00:28 +0530

    Created EC2 instances and DNS Records using terraform

--- remote-state/main.tf
+++ remote-state/main.tf
@@ -2,7 +2,7 @@ terraform {
       backend "s3" {
       bucket = "kgs-terraform"
       key = "test/terraform.tfstate"
-      region = "us-east1"
+      region = "us-east-1"
     }
 }
  resource "null_resource" "test" {}
